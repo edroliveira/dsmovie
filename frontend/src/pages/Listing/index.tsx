f68243354eb0e40e2b8d22efebc181f6f41ff271
Ednaldo Romer <ednaldojunior2001@gmail.com>
2022-01-16 21:53:23 -0300

    useParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`).then(resp => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`).then(resp => {
             const data = resp.data as MoviePage;
             setPage(data);
         });
